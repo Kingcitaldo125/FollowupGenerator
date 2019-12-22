4389be21ab3064f2c5e3a606eff53c3b60c08322
Paula <paularelt@gmail.com>
2019-12-22 16:14:15 -0500

    Adding GitHub info

--- thanks.py
+++ thanks.py
@@ -4,6 +4,9 @@ import configparser
 #includeLinkedIn = False
 includeLinkedIn = True
 
+#includeGithub = False
+includeGithub = True
+
 cnfg = configparser.ConfigParser()
 cnfg.read('configfile.ini')
 
@@ -12,13 +15,22 @@ company = str(cnfg['SectionOne']['COMPANY'])
 role = str(cnfg['SectionOne']['ROLE'])
 email = str(cnfg['SectionOne']['EMAIL'])
 linkedin = str(cnfg['SectionOne']['LINKEDIN'])
+github = str(cnfg['SectionOne']['GITHUB'])
 
 genStr = ""
 templateHolder=""
 
 with open('template.txt', 'r') as f:
 	for ff in f.readlines():
-		if not includeLinkedIn:
+		if not includeLinkedIn and not includeGithub:
+			m = re.search(r'LinkedIn|GitHub', ff, re.M|re.I)
+			if not m: # if the line is not the linkedin line, add it to the holder
+				templateHolder += str(ff)
+		elif not includeGithub:
+			m = re.search(r'GitHub', ff, re.M|re.I)
+			if not m: # if the line is not the linkedin line, add it to the holder
+				templateHolder += str(ff)
+		elif not includeLinkedIn:
 			m = re.search(r'LinkedIn', ff, re.M|re.I)
 			if not m: # if the line is not the linkedin line, add it to the holder
 				templateHolder += str(ff)
@@ -31,6 +43,9 @@ templateHolder = re.sub(r'\[email\]', email, templateHolder, re.M|re.I)
 
 if includeLinkedIn:
 	templateHolder = re.sub(r'\[linkedin\]', linkedin, templateHolder, re.M|re.I)
+
+if includeGithub:
+	templateHolder = re.sub(r'\[github\]', github, templateHolder, re.M|re.I)
 	
 templateHolder = re.sub(r'\[name\]', name, templateHolder, re.M|re.I)
 
